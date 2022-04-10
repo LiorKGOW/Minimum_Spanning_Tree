409042f24560e4e9177db0bb2528200076cf5b47
Lior Keren <liorkgow@gmail.com>
2022-04-10 23:12:47 +0300

    fixing graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,40 +1,49 @@
 #include "Graph.h"
 
-Graph:: Graph(int n) {
+Graph::Graph(int n)
+{
 
 	MakeEmptyGraph(n);
 }
 
-void Graph::MakeEmptyGraph(int n) {
+void Graph::MakeEmptyGraph(int n)
+{
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 
 		this->adjList.push_back(new List());
 	}
 }
 
-bool Graph::IsAdjacent(int ver1, int ver2) {
+bool Graph::IsAdjacent(int ver1, int ver2)
+{
 
 	return adjList[ver1]->isInList(ver2);
 }
 
-List* Graph::GetAdjList(int vertex) {
+List *Graph::GetAdjList(int vertex)
+{
 
 	return adjList[vertex];
 }
 
-void Graph::AddEdge(int ver1, int ver2, int weight) {
+void Graph::AddEdge(int ver1, int ver2, int weight)
+{
 
-	adjList[ver1] -> insertToTail(ver2, weight);
+	adjList[ver1]->insertToTail(ver2, weight);
 }
 
-void Graph::RemoveEdge(int ver1, int ver2) {
-	
-	if (!(adjList[ver1]->isInList(ver2))) {
+void Graph::RemoveEdge(int ver1, int ver2)
+{
 
-		cout << "The Edge<" << ver1 + 1 << "," << ver2 + 1 << "> is not in the graph" << endl;
+	if (!(adjList[ver1]->isInList(ver2)))
+	{
+
+		cout << "The Edge (" << ver1 + 1 << "," << ver2 + 1 << ") is not in the graph" << endl;
 	}
-	else { 
+	else
+	{
 
 		adjList[ver1]->removeFromList(ver2);
 		adjList[ver2]->removeFromList(ver1);
