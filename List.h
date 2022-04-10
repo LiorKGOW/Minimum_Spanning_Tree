dd815a58972656e7b634eb0f7a42d65cfa81f5cd
Lior Keren <liorkgow@gmail.com>
2022-04-10 22:47:19 +0300

    Adding List Class

---
+++ List.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Node.h"
+
+class List
+{
+private:
+	Node* head;
+	Node* tail;
+
+public:
+	List();
+	~List();
+
+	void makeEmptyList();
+	bool isEmpty();
+	void removeFromList(int val);
+	bool isInList(int val);
+
+	// Getters:
+
+	Node* getHead();
+	Node* getTail();
+
+	// Setters:
+
+	void insertToHead(int vertex, int weight);
+	void insertToTail(int vertex, int weight);
+};
+
