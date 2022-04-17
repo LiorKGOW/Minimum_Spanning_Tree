56f21e0c2f1f16a643a935b74327d0699e2e93b8
Lior Keren <liorkgow@gmail.com>
2022-04-17 17:42:05 +0300

    Adding UnionVertices & Fixing Find
    printUnion init

--- Union.cpp
+++ Union.cpp
@@ -1,38 +1,79 @@
 #include "Union.h"
 
-Union::Union(int n) {
+/*********************************************************************/
 
-	for (int i = 0; i < n; i++) {
-		
+Union::Union(int n)
+{
+
+	this->size = n;
+
+	for (int i = 0; i < n; i++)
+	{
+
 		MakeSet(i);
 	}
 }
 
-void Union::MakeSet(int ver) {
+/*********************************************************************/
 
-	arr.push_back(treeNode{ ver, ver, 1 });
+void Union::MakeSet(int vertex)
+{
+
+	arr.push_back(treeNode{vertex, vertex, 1});
 }
 
-int Union::Find(int ver) {
+/*********************************************************************/
+/*
+ * Find is a recursive function.
+ * The function returns the representative of ver.
+ * With Path Compression
+ */
+int Union::Find(int vertex)
+{
 
-	if (arr[ver].parent == ver) {
+	// Stop Condition
+	if (arr[vertex].parent == vertex)
+	{
 
-		return ver;
+		return vertex;
 	}
 
-	int parent = Find(arr[ver].parent);
-	arr[ver].parent = parent;
-	return parent;
+	// recursive call
+	int representative = Find(arr[vertex].parent);
 
-	/*int curr = ver;
+	arr[vertex].parent = representative; // path compression
 
-	while (arr[curr].parent != curr) {
-
-		int temp = arr[curr].parent;
-		arr[curr].parent = ver;
-		curr = temp;
-	}
-
-	return curr;*/
+	return representative;
 }
-void Union:: UnionVertices(int ver1, int ver2);
+
+/*********************************************************************/
+/*
+ * With Union By Size
+ * ASSUMPTION: ver1 and ver2 are representatives of their Set.
+ */
+void Union::UnionVertices(int ver1, int ver2) // TODO: implement
+{
+	if (ver1 == ver2)
+		return;
+
+	if (arr[ver1].size >= arr[ver2].size)
+	{
+
+		arr[ver2].parent = ver1;
+	}
+	else
+	{ // arr[Find(ver1)].size < arr[Find(ver2)].size
+
+		arr[ver1].parent = ver2;
+	}
+}
+
+/*********************************************************************/
+/*
+ * {/?rep: ?/1, 2, 3}, { rep: 4,5 }, { rep: 6 }
+ */
+void Union::printUnion()
+{ // TODO: implement
+
+	// for(int i=0;i<)
+}
