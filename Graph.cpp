da8e138d4f9eadbf29ee0b47f5cf5d1dca3e5b92
Lior Keren <liorkgow@gmail.com>
2022-04-22 23:56:00 +0300

    Graph: added DFS Funcitons & Attributes

--- Graph.cpp
+++ Graph.cpp
@@ -5,16 +5,26 @@
 Graph::Graph(int n)
 {
 	this->adjList.reserve(n);
-	this->size = n;
+	this->Num_of_Vertices = n;
 
 	MakeEmptyGraph(n);
+
+	this->colors.reserve(n);
+
+	for (int i = 0; i < Num_of_Vertices; i++)
+	{
+		colors.push_back(WHITE);
+	}
+
+	this->colorsIsWhite = true;
 }
 
 /*********************************************************************/
-//Destructor
-Graph:: ~Graph() {
-
-	for (int i = 0; i < this->size; i++) {
+// Destructor
+Graph::~Graph()
+{
+	for (int i = 0; i < this->Num_of_Vertices; i++)
+	{
 
 		delete adjList[i];
 	}
@@ -30,9 +40,9 @@ void Graph::MakeEmptyGraph(int n)
 }
 
 /*********************************************************************/
-/* 
-* Checks if the edge (ver1, ver2) is in the graph.
-*/
+/*
+ * Checks if the edge (ver1, ver2) is in the graph.
+ */
 bool Graph::IsAdjacent(int ver1, int ver2)
 {
 	return adjList[ver1 - 1]->isInList(ver2 - 1);
@@ -40,22 +50,23 @@ bool Graph::IsAdjacent(int ver1, int ver2)
 
 /*********************************************************************/
 /*
-* GetAdjList returns a *duplication* of the adjList of vertex.
-*/
-List* Graph::GetAdjList(int vertex)
+ * GetAdjList returns a *duplication* of the adjList of vertex.
+ */
+List *Graph::GetAdjList(int vertex)
 {
-	List* result = adjList[vertex - 1]->duplicateList();
+	List *result = adjList[vertex - 1]->duplicateList();
 
 	return result;
 }
 
 /*********************************************************************/
 /*
-* AddEdge Adds the edge (ver1, ver2) to the graph.
-*/
+ * AddEdge Adds the edge (ver1, ver2) to the graph.
+ */
 void Graph::AddEdge(int ver1, int ver2, int weight)
 {
-	if (!IsAdjacent(ver1, ver2)) {
+	if (!IsAdjacent(ver1, ver2))
+	{
 
 		adjList[ver1 - 1]->insertToTail(ver2 - 1, weight);
 
@@ -67,11 +78,12 @@ void Graph::AddEdge(int ver1, int ver2, int weight)
 
 /*********************************************************************/
 /*
-* AddEdge Adds the edge (ver2, ver1) to the graph.
-*/
-void Graph:: AddInvertedEdge(int ver2, int ver1, int weight) {
-
-	if (!IsAdjacent(ver2, ver1)) {
+ * AddEdge Adds the edge (ver2, ver1) to the graph.
+ */
+void Graph::AddInvertedEdge(int ver2, int ver1, int weight)
+{
+	if (!IsAdjacent(ver2, ver1))
+	{
 
 		adjList[ver2 - 1]->insertToTail(ver1 - 1, weight);
 	}
@@ -81,10 +93,10 @@ void Graph:: AddInvertedEdge(int ver2, int ver1, int weight) {
 
 /*********************************************************************/
 /*
-* Removes the Edges: (ver1, ver2) & (ver2, ver1) from the graph.
-* 
-* ASSUMPTION: if the edge (ver1, ver2) is in the graph, the inverted edge (ver2, ver1) is also in the graph.
-*/
+ * Removes the Edges: (ver1, ver2) & (ver2, ver1) from the graph.
+ *
+ * ASSUMPTION: if the edge (ver1, ver2) is in the graph, the inverted edge (ver2, ver1) is also in the graph.
+ */
 void Graph::RemoveEdge(int ver1, int ver2)
 {
 	if (!IsAdjacent(ver1, ver2))
@@ -98,17 +110,90 @@ void Graph::RemoveEdge(int ver1, int ver2)
 	}
 }
 
+///*********************************************************************/
+// void Graph::DFS()
+//{
+//	if (!this->colorsIsWhite)
+//		makeColorsWhite();
+//
+//	for (int i = 0; i < Num_of_Vertices; i++)
+//	{
+//		if (colors[i] == WHITE)
+//			visit(i);
+//	}
+//
+//	this->colorsIsWhite = false;
+// }
+
+/*********************************************************************/
+void Graph::makeColorsWhite()
+{
+	for (int i = 0; i < Num_of_Vertices; i++)
+	{
+		colors[i] = WHITE;
+	}
+
+	this->colorsIsWhite = true;
+}
+
+/*********************************************************************/
+
+void Graph::visit(int vertex)
+{
+	colors[vertex] = GRAY;
+
+	Node *curr = adjList[vertex]->getHead();
+
+	while (curr != nullptr)
+	{
+		if (colors[curr->getVertex()] == WHITE)
+		{
+			colors[curr->getVertex()] = GRAY;
+			visit(curr->getVertex());
+		}
+
+		curr = curr->getNext();
+	}
+
+	colors[vertex] = BLACK;
+}
+
+/*********************************************************************/
+bool Graph::isConnectedGraph()
+{
+	if (!this->colorsIsWhite)
+		makeColorsWhite();
+
+	visit(0);
+
+	this->colorsIsWhite = false;
+
+	for (int i = 0; i < Num_of_Vertices; i++)
+	{
+		if (colors[i] != BLACK)
+			return false;
+	}
+
+	return true;
+}
+
 /*********************************************************************/
 /*
-* in case a vertex doesn't have edges attached to it, printGraph prints an empty line.
-*/
-void Graph:: printGraph() {
-
-	for (int i = 0; i < size; i++)
+ * in case a vertex doesn't have edges attached to it, printGraph prints an empty line.
+ */
+void Graph::printGraph()
+{
+	for (int i = 0; i < Num_of_Vertices; i++)
 	{
-		adjList[i]->printList(i+1);
+		adjList[i]->printList(i + 1);
 		cout << endl;
 	}
+}
+
+/*********************************************************************/
+int Graph::get_Num_of_Vertices()
+{
+	return this->Num_of_Vertices;
 }
 
 /*********************************************************************/
--- Graph.h
+++ Graph.h
@@ -5,32 +5,45 @@
 
 using namespace std;
 
+// Consts for the DFS functions:
+
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
+
 class Graph
 {
 	// The vertexes are 0 - n-1, we will treat them as 1 - n
 	// in case a vertex doesn't have edges attached to it, printGraph prints an empty line.
 
-	// TODO: if Graph size is 0? -> throw exception? / deal with the kelet
-	// TODO: if addEdge(3,3) -> Error?
-	// TODO: adding an existing edge -> Error?
-	// TODO: trying to remove unexisting edge from the graph -> Error?  (We print an error message and continuing as normal
-
 private:
+	vector<List *> adjList;
+	int Num_of_Vertices;
 
-	vector<List*> adjList;
-	int size;
-	
+	vector<int> colors; // in use in DFS functions
+	bool colorsIsWhite;
+
 public:
-	Graph(int n);  // Constructor
-	~Graph();      // Destructor
+	Graph(int n); // Constructor
+	~Graph();	  // Destructor
 
 	void MakeEmptyGraph(int n);
 	bool IsAdjacent(int ver1, int ver2);
-	List* GetAdjList(int vertex);
+	List *GetAdjList(int vertex);
 	void AddEdge(int ver1, int ver2, int weight);
 	void AddInvertedEdge(int ver2, int ver1, int weight);
 	void RemoveEdge(int ver1, int ver2);
 
-	void printGraph();
-	// in case a vertex doesn't have edges attached to it, printGraph prints an empty line.
+	// void DFS();
+	void makeColorsWhite();
+	void visit(int vertex);
+	bool isConnectedGraph();
+
+	void printGraph(); // in case a vertex doesn't have edges attached to it, printGraph prints an empty line.
+
+	// Getters:
+
+	int get_Num_of_Vertices();
+
+	// Setters:
 };
